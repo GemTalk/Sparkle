adb1ee72dadf383324bb72847637043a285c5144
Martin McClure <martin@hand2mouse.com>
2020-12-24 15:35:11 -0800

    Refactor GemStone scripts, more like RSR ones now

---
+++ src-gs/bootstrapSparkle.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+### Install RSR and Sparkle from .gs files into a non-Rowan stone
+
+topaz -I login.topaz  -S bootstrapSparkle.topaz
---
+++ src-gs/generateGs.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+## Generate bootstrap.gs file from a Rowan-enabled stone with Sparkle loaded.
+
+topaz -I login.topaz  -S generateGs.topaz

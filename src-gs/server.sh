421214512b50f8ef5addc494defaa381ec56d0b4
Martin McClure <martin@hand2mouse.com>
2020-12-28 19:15:52 -0800

    Update server script to log in as SystemUser

--- src-gs/server.sh
+++ src-gs/server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-topaz -I login.topaz  -S ${ROWAN_PROJECTS_HOME}/RemoteServiceReplication/scripts/runServer.topaz
+topaz -I loginSystemUser.topaz  -S ${ROWAN_PROJECTS_HOME}/RemoteServiceReplication/scripts/runServer.topaz

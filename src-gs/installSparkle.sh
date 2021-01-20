cbffc99d9850ea24f28922b7b942a31b40feadbb
Martin McClure <martin@hand2mouse.com>
2021-01-19 18:28:23 -0800

    Make install script exit with 1 on compiler warning

--- src-gs/installSparkle.sh
+++ src-gs/installSparkle.sh
@@ -2,4 +2,14 @@
 ### Install RSR and Sparkle from Tonel files into a Rowan-enabled stone
 ### Check for error count of 0 at end
 
-topaz -I loginSystemUser.topaz  -S installSparkle.topaz
+## Topaz refuses to exit from script if input is stdin, so redirect from /dev/zero
+topaz -I loginSystemUser.topaz  -S installSparkle.topaz < /dev/zero
+if [ $? = 0 ]
+    then
+        exit 0
+    else
+        echo !!!!!!!!!!!!!!
+        echo INSTALL FAILED
+        echo !!!!!!!!!!!!!!
+        exit 1
+    fi

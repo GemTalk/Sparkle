ae8fcf93229810c17d064d4c7b0ac66cb7397f14
Martin McClure <martin@hand2mouse.com>
2021-02-16 19:05:15 -0800

    Script for running tests with or without Rowan

---
+++ src-gs/runTestsInRowan.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+### Run Sparkle tests in a Rowan-enabled stone
+
+spkHome=${ROWAN_PROJECTS_HOME}/Sparkle
+## Topaz refuses to exit from script if input is stdin, so redirect from /dev/zero
+topaz -I ${spkHome}/src-gs/login.topaz  -S ${spkHome}/src-gs/runTestsInRowan.topaz < /dev/zero
+if [ $? = 0 ]
+    then
+        exit 0
+    else
+        echo !!!!!!!!!!!!!!
+        echo TESTS FAILED
+        echo !!!!!!!!!!!!!!
+        exit 1
+    fi

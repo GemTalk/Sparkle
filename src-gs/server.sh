32240e6863527f75271d68eb18be9b0a9ff010c6
Martin McClure <martin@hand2mouse.com>
2021-03-03 19:48:56 -0800

    Server script loops, Issue 48

--- src-gs/server.sh
+++ src-gs/server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-topaz -l -I login.topaz  -S ${ROWAN_PROJECTS_HOME}/Sparkle/src-gs/runServer.topaz
+topaz -l -I login.topaz  -S ${ROWAN_PROJECTS_HOME}/Sparkle/src-gs/runServerLoop.topaz
